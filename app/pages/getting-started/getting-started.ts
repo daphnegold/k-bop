38ff0da8f93031fe192d6befe5e13021b6a92f1a
Daphne <dg1181@nyu.edu>
2016-02-21 19:50:35 -0800

    Initial Commit

---
+++ app/pages/getting-started/getting-started.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+
+  }
+}
---
+++ app/pages/grid/grid.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'build/pages/grid/grid.html'
+})
+export class GridPage {
+  constructor(){
+
+  }
+}
