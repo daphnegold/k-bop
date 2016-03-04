bee3f5fc13fdf5c532199a06811bc84b0bbdc9b7
Daphne <dg1181@nyu.edu>
2016-03-04 13:42:08 -0800

    successfully submitting comments to api

---
+++ app/comment.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from 'angular2/core';
+import {Song} from './song';
+import {Headers, RequestOptions} from 'angular2/http';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import {Storage, LocalStorage} from 'ionic-framework/ionic';
+import 'rxjs/Rx';
+
+@Injectable()
+export class CommentService {
+  private _commentUrl = "http://kbop.herokuapp.com/comment/"
+
+  constructor (private http: Http) { }
+
+  addComment(song, commentText) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let body = JSON.stringify({ "data": { uri: song.uri, comment: commentText } });
+
+    return this.http.post(this._commentUrl, body, options)
+        .map(res => res.json())
+        .catch(this.handleError)
+  }
+
+  private handleError (error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+}
--- app/pages/comments/comments.ts
+++ app/pages/comments/comments.ts
@@ -1,9 +1,11 @@
 import {Page, ViewController, NavParams} from 'ionic-framework/ionic';
 import {FormBuilder, ControlGroup, Validators, AbstractControl} from 'angular2/common';
 import {Song} from '../../song';
+import {CommentService} from '../../comment.service';
 
 @Page({
   templateUrl: 'build/pages/comments/comments.html',
+  providers: [CommentService]
 })
 export class CommentsModal {
   commentForm: ControlGroup;
@@ -11,7 +13,11 @@ export class CommentsModal {
   song: Song;
   // active: boolean = true;
 
-  constructor(private viewCtrl: ViewController, private params: NavParams, private form: FormBuilder) {
+  constructor(private viewCtrl: ViewController,
+    private params: NavParams,
+    private form: FormBuilder,
+    private _commentService: CommentService) {
+
     this.song = this.params.get('mySong');
 
     this.commentForm = form.group({
@@ -21,8 +27,21 @@ export class CommentsModal {
     this.comment = this.commentForm.controls['comment'];
   }
 
+  addComment(comment) {
+    this._commentService.addComment(this.song, comment)
+      .subscribe(
+        data => {
+          console.log("Server response:")
+          console.log(data)
+        },
+        error => { console.log(<any>error); alert("Something has gone wrong, please try again later"); }
+      );
+   }
+
   onSubmit(value: string) {
     if(this.commentForm.valid) {
+      this.addComment(value["comment"]);
+
       console.log('Submitted value: ', value);
       this.dismiss();
       // this.active = false;
