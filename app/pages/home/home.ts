4b555b442fc731e24bfc39f948c60cee1ba3a3fb
Daphne <dg1181@nyu.edu>
2016-03-06 13:58:50 -0800

    modified some messed up indenting

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -28,146 +28,146 @@ export class HomePage {
     this.nav.present(modal)
   }
 
-   onPageWillEnter() {
-     this._hammerService.swipeInit();
-     this._hammerService.hammertime.on('swiperight', (event) => {
-       if (this._songService.songs) {
-         console.log('Swipe right');
-         setTimeout(() => {
-           this.hideImage = true;
-         }, 500);
-         this.currentSong.decided = true;
-         this.currentSong.choice = true;
-         this.decide(true);
-       }
-     });
-     this._hammerService.hammertime.on('swipeleft', (event) => {
-       if (this._songService.songs) {
-         console.log('Swipe left');
-         setTimeout(() => {
-           this.hideImage = true;
-         }, 500);
-         this.currentSong.decided = true;
-         this.currentSong.choice = false;
-         this.decide(false);
-       }
-     });
+  onPageWillEnter() {
+    this._hammerService.swipeInit();
+    this._hammerService.hammertime.on('swiperight', (event) => {
+      if (this._songService.songs) {
+        console.log('Swipe right');
+        setTimeout(() => {
+          this.hideImage = true;
+        }, 500);
+        this.currentSong.decided = true;
+        this.currentSong.choice = true;
+        this.decide(true);
+      }
+    });
+    this._hammerService.hammertime.on('swipeleft', (event) => {
+      if (this._songService.songs) {
+        console.log('Swipe left');
+        setTimeout(() => {
+          this.hideImage = true;
+        }, 500);
+        this.currentSong.decided = true;
+        this.currentSong.choice = false;
+        this.decide(false);
+      }
+    });
 
-     console.log(this._hammerService.hammertime);
+    console.log(this._hammerService.hammertime);
 
-     if (this._songService.songs) {
-       this._songService.removeAudio();
-       this.currentSong = this._songService.songs[0];
+    if (this._songService.songs) {
+      this._songService.removeAudio();
+      this.currentSong = this._songService.songs[0];
 
-       if (!this._songService.audio) {
-         this.songTime();
-       }
-     }
-   }
+      if (!this._songService.audio) {
+        this.songTime();
+      }
+    }
+  }
 
-   onPageWillLeave() {
-     console.log("leaving page")
-     this._hammerService.swipeDestroy();
-   }
+  onPageWillLeave() {
+    console.log("leaving page")
+    this._hammerService.swipeDestroy();
+  }
 
-   getSongs() {
-     if (this._songService.songs) {
-       console.log(this._songService.songs.length)
-     }
+  getSongs() {
+    if (this._songService.songs) {
+      console.log(this._songService.songs.length)
+    }
 
-     if (!this._songService.songs || this._songService.songs.length < 10) {
+    if (!this._songService.songs || this._songService.songs.length < 10) {
 
-       this._songService.getSongs()
-          .subscribe(
-            songs => {
-              this._songService.songs = songs;
-              if (!this.currentSong) {
-                this.currentSong = this._songService.songs[0];
-
-                if (!this._songService.audio) {
-                  this.songTime();
-                }
-              }
-
-              console.log(this.currentSong)},
-            error => { console.log(<any>error); alert("Something has gone wrong, please try again later"); }
-          );
-     } else {
-       this.currentSong = this._songService.songs[0];
-
-       if (!this._songService.audio) {
-         this.songTime();
-       }
-     }
-   }
-
-   toggleSong() {
-     if (this._songService.played) {
-       this._songService.played = false;
-
-       this.songTime();
-     } else {
-       this._songService.toggleSong();
-     }
-   }
-
-   stopSong() {
-     this._songService.stopSong();
-   }
-
-   removeAudio() {
-     this._songService.removeAudio();
-   }
-
-   startSong() {
-     this._songService.startSong();
-   }
-
-   playSong() {
-     return this._songService.playSong(this.currentSong);
-   }
-
-   decide(choice) {
-     this._songService.songs.splice(this._songService.songs.indexOf(this.currentSong), 1);
-     console.log(this._songService.songs.length);
-
-     if (this._songService.songs.length < 10) {
-       this.getSongs();
-     }
-
-     if (choice) {
-       let addedSong = this.currentSong
-       this._songService.addSong(addedSong.uri)
+      this._songService.getSongs()
          .subscribe(
-           data => {
-             this._playlistService.addSong(addedSong);
-             console.log("Server response:")
-             console.log(data)
-           },
+           songs => {
+             this._songService.songs = songs;
+             if (!this.currentSong) {
+               this.currentSong = this._songService.songs[0];
+
+               if (!this._songService.audio) {
+                 this.songTime();
+               }
+             }
+
+             console.log(this.currentSong)},
            error => { console.log(<any>error); alert("Something has gone wrong, please try again later"); }
          );
-     }
+    } else {
+      this.currentSong = this._songService.songs[0];
 
-     let randomNumber = Math.round(Math.random() * (this._songService.songs.length - 1));
-     let nextSong = this._songService.songs[randomNumber];
-     setTimeout(() => {
-       this.hideImage = false;
-       this.currentSong = nextSong;
-       this.songTime();
-     }, 500);
-   }
+      if (!this._songService.audio) {
+        this.songTime();
+      }
+    }
+  }
 
-   songTime() {
-     this.playSong().then((success) => {
-       this._songService.played = true;
-       this._songService.stopSong();
-       console.log("Preview complete")
-     }, (error) => {
-       alert("Something has gone wrong");
-       this.removeAudio();
-       console.error(error);
-     });
-   }
+  toggleSong() {
+    if (this._songService.played) {
+      this._songService.played = false;
+
+      this.songTime();
+    } else {
+      this._songService.toggleSong();
+    }
+  }
+
+  stopSong() {
+    this._songService.stopSong();
+  }
+
+  removeAudio() {
+    this._songService.removeAudio();
+  }
+
+  startSong() {
+    this._songService.startSong();
+  }
+
+  playSong() {
+    return this._songService.playSong(this.currentSong);
+  }
+
+  decide(choice) {
+    this._songService.songs.splice(this._songService.songs.indexOf(this.currentSong), 1);
+    console.log(this._songService.songs.length);
+
+    if (this._songService.songs.length < 10) {
+      this.getSongs();
+    }
+
+    if (choice) {
+      let addedSong = this.currentSong
+      this._songService.addSong(addedSong.uri)
+        .subscribe(
+          data => {
+            this._playlistService.addSong(addedSong);
+            console.log("Server response:")
+            console.log(data)
+          },
+          error => { console.log(<any>error); alert("Something has gone wrong, please try again later"); }
+        );
+    }
+
+    let randomNumber = Math.round(Math.random() * (this._songService.songs.length - 1));
+    let nextSong = this._songService.songs[randomNumber];
+    setTimeout(() => {
+      this.hideImage = false;
+      this.currentSong = nextSong;
+      this.songTime();
+    }, 500);
+  }
+
+  songTime() {
+    this.playSong().then((success) => {
+      this._songService.played = true;
+      this._songService.stopSong();
+      console.log("Preview complete")
+    }, (error) => {
+      alert("Something has gone wrong");
+      this.removeAudio();
+      console.error(error);
+    });
+  }
 
   doRefresh(refresher) {
     if (this.pullable) {
